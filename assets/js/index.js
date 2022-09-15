70c3a60ce1e3f8b3cea8534903ce6035ea425662
Stefaniia Bodnia <81180702+StefaBodnia@users.noreply.github.com>
2022-09-15 18:08:17 +0200

    3 Exercices
    Done!

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,26 @@ const helloWorld = () => {
 };
 
 helloWorld();
+
+document.querySelectorAll(".list__item__header").forEach((item) => {
+ item.addEventListener("click",()=>
+ { if (item.nextElementSibling.style.display==="none") {
+      item.nextElementSibling.style.display = "block"
+ } else {
+  item.nextElementSibling.style.display="none"
+}
+    console.log("list item is clicked");
+  });
+});
+
+
+document.querySelectorAll('.list__item__artist').forEach(item => {
+  let html = item.innerHTML;
+  html = html.replace(/\D/g,'');
+  item.innerHTML = html;
+})
+
+
+// document.getElementById(".list__item__artist").innerHTML = "date of birth" {
+
+// }
